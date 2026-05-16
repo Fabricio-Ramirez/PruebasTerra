fc4769b68533df246ebe14eedaa6f4f5fcb8b3b1
Fabricio-Ramirez <fr327619@fi365.ort.edu.uy>
2026-05-16 13:26:06 -0300

    Add user data script to install Git and Terraform on AWS instance

--- TerraBastionAuto/modules/custom-module/module.tf
+++ TerraBastionAuto/modules/custom-module/module.tf
@@ -3,6 +3,12 @@ resource "aws_instance" "module-instance-deploy" {
     key_name = var.key_name
     ami = var.ami
     iam_instance_profile = "LabInstanceProfile"
+    user_data = <<-EOF
+                #!/bin/bash
+                yum install git -y
+                sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+                sudo yum install terraform -y
+                EOF
     tags = {
         Name = var.name_instance
     }
