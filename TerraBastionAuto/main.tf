6911db2d86154a03e826763720b3a72a8d8214b5
Fabricio-Ramirez <fr327619@fi365.ort.edu.uy>
2026-05-20 11:38:16 -0300

    Refactor EC2 module: rename custom-module to ec2-module and migrate resources

--- TerraBastionAuto/main.tf
+++ TerraBastionAuto/main.tf
@@ -1,5 +1,5 @@
-﻿module "custom-module" {
-    source = "./modules/custom-module"
+﻿module "ec2-module" {
+    source = "./modules/ec2-module"
     ami = var.ami_input
     instance_type_input = var.instance_type_input
     key_name = var.key_name_input
@@ -7,5 +7,5 @@
 }
 
 output "dns-output" {
-    value = module.custom-module.dns
+    value = module.ec2-module.dns
 }
